16c25e30808a7c2b02462970dc2b081e483e14a1
Alaminxab <alaminxab@gmail.com>
2022-07-17 20:37:27 +0100

    rainbow kit test

---
+++ my-app/pages/_app.js
@@ -0,0 +1,41 @@
+import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig, } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+
+const { chains, provider } = configureChains(
+  [ chain.polygonMumbai, chain.mainnet, chain.polygon,],
+  [
+    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+
+
+function MyApp({ Component, pageProps }) {
+  return (    
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider theme={darkTheme()} chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
+
+export default MyApp
